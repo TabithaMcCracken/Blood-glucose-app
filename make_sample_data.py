e060fd6b78396ee1b3a7ed4fc42446845e2f975f
Tabitha <tabitha.r.mccracken@gmail.com>
2023-11-08 13:01:24 -0700

    Changes to Added further condensing of data.

--- make_sample_data.py
+++ make_sample_data.py
@@ -1,64 +1,58 @@
 import random
 
-def generate_glucose_levels():
-    # Constants
-    MIN_GLUCOSE = 60
-    MAX_GLUCOSE = 250
-    MIN_CHANGE = -10
-    MAX_CHANGE = 10
-    TIME_INCREMENT = 15   # Update to 15 minutes
+# Constants for generating glucose levels
+MIN_GLUCOSE = 60
+MAX_GLUCOSE = 250
+MIN_CHANGE = -10
+MAX_CHANGE = 10
+TIME_INCREMENT = 5
+DATE = "23-08-01"
 
-    # Start in the middle range
+# Constants for display
+MIN_GLUCOSE = 60
+SCALE_FACTOR = 0.2  # Set the length of the bars
+
+def generate_glucose_levels():
+
+    # Start in the middle of the range of data
     current_glucose = (MIN_GLUCOSE + MAX_GLUCOSE) // 2
     previous_glucose = None
     glucose_data = []
 
     for h in range(24):
         for m in range(0, 60, TIME_INCREMENT):
-            # Randomly decide the change in glucose (ensuring it's within limits)
+            # Randomly decide the change in glucose (within limits)
             glucose_change = random.randint(MIN_CHANGE, MAX_CHANGE)
             # Ensure next value is within allowed range
             current_glucose = min(max(MIN_GLUCOSE, current_glucose + glucose_change), MAX_GLUCOSE)
 
             if current_glucose != previous_glucose:  # Only store if different from previous
-                entry = f"{h:02}:{m:02}|{current_glucose}"
+                time = f"{h:02}:{m:02}"
+                entry = f"{DATE}|{time}|{current_glucose}"
                 glucose_data.append(entry)
                 previous_glucose = current_glucose
 
     return glucose_data
 
-# def display_glucose_levels(data):
-#     print("Time  | Blood Glucose Level")
-#     print("------" + "-" * 20)
-
-#     for entry in data:
-#         try:
-#             time, level = entry.split('|')
-#             print(f"{time} | {level}")
-#         except ValueError:
-#             print(f"Error with entry: {entry}")
-
 def display_glucose_levels(data):
-    # Constants for display
-    MIN_GLUCOSE = 60
-    SCALE_FACTOR = 0.2  # This will determine the length of bars
 
-    print("Time  | Glucose Level | Graph")
-    print("------|---------------|--------------------")
+    print("Date       | Time  | Glucose | Graph")
+    print("-----------|-------|---------|--------------------")
 
     for entry in data:
-        time, level_str = entry.split('|')
+        date, time, level_str = entry.split('|')
         level = int(level_str)
 
-        # Calculate bar length based on glucose level and scale factor
+        # Calculates bar length based on glucose level and scale factor
         bar_length = int((level - MIN_GLUCOSE) * SCALE_FACTOR)
         bar = '#' * bar_length
 
-        print(f"{time} | {level:13} | {bar}")
+        print(f"{date} | {time} | {level:7} | {bar}")
 
 
 if __name__ == "__main__":
     # Testing the function with generated data
     data = generate_glucose_levels()
+    print(data)
     display_glucose_levels(data)
 
