f4d2dca0b59174a86114a4d135d01ceb9a742562
Tabitha <tabitha.r.mccracken@gmail.com>
2023-11-15 22:16:03 -0700

    change the data class and successfully got it to back up to the database woohoo

--- practice2.py
+++ practice2.py
@@ -13,31 +13,40 @@
 
 import csv
 from datetime import datetime
-import sqlite3
+import sqlalchemy
+from secret import password
+import pymysql
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float, MetaData
+from sqlalchemy.orm import declarative_base, Session
+
+# Define the SQLAlchemy model
+Base = declarative_base()
+
 
 file_path = "/Users/tabithamccracken/Documents/codingnomads/blood_glucose_app/cgm_data_one_day.csv"
+# Replace 'your_mysql_username', 'your_mysql_password', 'your_mysql_host', and 'your_mysql_database' with your MySQL credentials
+mysql_engine = create_engine(f"mysql+mysqlconnector://root:{password}@localhost/glucose_data")
 
-# Define a class to store the data
-class GlucoseData:
-    def __init__(self, id, name, date_range, timestamp, glucose_value):
-        self.id = id
-        self.name = name
-        self.date_range = date_range
-        self.timestamp = timestamp
-        self.glucose_value = glucose_value
+# sql_engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{password}@localhost/glucose_data')
+# connection = sql_engine.connect()
+# metadata = sqlalchemy.MetaData()
 
-# def load_data(self):
-#         with open(self.file_path, "r", encoding='utf-8') as file:
-#             csvreader = csv.reader(file)
-#             self.patient_info_line = next(csvreader)
-#             headers = next(csvreader)
 
-#             for row in csvreader:
-#                 time_stamp, glucose, _ = row  # We don't need the device info
-#                 self.data.append((time_stamp, glucose)) 
+class GlucoseData(Base):
+    __tablename__ = 'glucose_data'
 
-#             for item in self.data:
-#                 print(item)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    time_stamp = Column(DateTime)
+    glucose_value = Column(Float)
+
+# # Define a class to store the data
+# class GlucoseData:
+#     def __init__(self, id, name, timestamp, glucose_value):
+#         self.id = id
+#         self.name = name
+#         self.timestamp = timestamp
+#         self.glucose_value = glucose_value
 
 # Function to read CSV file and create GlucoseData objects
 def read_csv(file_path):
@@ -47,40 +56,52 @@ def read_csv(file_path):
         
         # Read metadata (skip the first line)
         metadata_line = next(reader)
-        name, date_range = None, None
+        patient_name = None
         for item in metadata_line:
             if item.startswith('Name:'):
-                name = item.split('Name:')[1].strip()
-            elif item.startswith('Date Range:'):
-                date_range = item.split('Date Range:')[1].strip()
+                patient_name = item.split('Name:')[1].strip()
 
         # Read actual column headers
         headers = next(reader)
         
         for row in reader:
-            timestamp = datetime.strptime(row[0], '%Y-%m-%d %H:%M')
+            time_stamp = datetime.strptime(row[0], '%Y-%m-%d %H:%M')
             glucose_value = float(row[1])
-            glucose_data = GlucoseData(id, name, date_range, timestamp, glucose_value)
+            glucose_data = GlucoseData(
+                name=patient_name,
+                time_stamp=time_stamp,
+                glucose_value=int(glucose_value)
+            )
             glucose_data_list.append(glucose_data)
     return glucose_data_list
 
 
-def insert_into_database(data_list):
-    connection = sqlite3.connect('/usr/local/mysql/data/glucose_data.db')
-    cursor = connection.cursor()
+# def insert_into_database(metadata, engine, connection, glucose_data):
+#     # Connect to local database
+#     if glucose_data:
+#         data_list = sqlalchemy.Table('glucose_data', metadata, autoload=True, autoload_with=engine)
+#         query = sqlalchemy.insert(data_list)
+#         result_proxy = connection.execute(query, glucose_data)
 
-    for data in data_list:
-        cursor.execute(
-            "INSERT INTO glucose_data (id, name, date_range, timestamp, glucose_value) VALUES (?, ?, ?, ?)",
-            (data.name, data.date_range, data.timestamp, data.glucose_value)
-        )
+#     else:
+#         print("There was no data, so no data was added to the database.")
 
-    connection.commit()
-    connection.close()
 
+def insert_into_database(data_list, engine):
+    Base.metadata.create_all(engine)
+    with Session(engine) as session:
+        for data in data_list:
+            session.add(data)
+        session.commit()
+    
 if __name__ == "__main__":
     # Create instance of class with the current file path
     glucose_data_list = read_csv(file_path)
 
+    # for item in glucose_data_list:
+    #     print(f"Name: {item.name}")
+    #     print(f"Timestamp: {item.time_stamp}")
+    #     print(f"Glucose Value: {item.glucose_value}")
+
     if glucose_data_list:
-        insert_into_database(glucose_data_list)
+        insert_into_database(glucose_data_list, mysql_engine)
