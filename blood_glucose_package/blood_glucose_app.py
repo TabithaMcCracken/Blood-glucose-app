e1e6b3b2fac48a1849f5f3a55cde37699e5f488d
Tabitha <tabitha.r.mccracken@gmail.com>
2024-01-10 22:03:56 -0700

    Cleaned up code

--- blood_glucose_package/blood_glucose_app.py
+++ blood_glucose_package/blood_glucose_app.py
@@ -1,12 +1,8 @@
-# This application allows the user to store blood glucose data to a SQL database, 
-# get and plot the last 24 hours of data from the database, or ask ChatGPT to analyze 
-# the last 24 hours of data
-
+import os
 
 import csv
 from datetime import datetime, timedelta
 import sqlalchemy
-# from secret import password
 import pymysql
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Float, MetaData
 from sqlalchemy.orm import declarative_base, Session
@@ -20,27 +16,14 @@ import matplotlib.ticker as ticker
 import matplotlib.dates as mdates
 import openai
 from token_count import num_tokens_from_string
-# import gzip
-# import json
 from open_ai_chat import chat
 import zlib
 import base64
-import os
-
 
 file_path = "/Users/tabithamccracken/Documents/codingnomads/blood_glucose_app/blood_glucose_package/cgm_data_one_week.csv"
 
-
 # Define the SQLAlchemy model
 Base = declarative_base()
-
-# Create the MySQL engine
-database_password = os.environ.get('MYSQL_PASSWORD')
-mysql_engine = create_engine(f"mysql+mysqlconnector://root:{database_password}@localhost/glucose_data")
-
-# OpenAI API key
-openai_key = os.environ.get('OPENAI_KEY')
-openai.api_key = openai_key
 
 class GlucoseData(Base):
     __tablename__ = 'glucose_data'
@@ -51,7 +34,16 @@ class GlucoseData(Base):
     glucose_value = Column(Float)
     
 # Function to read CSV file and create GlucoseData objects
-def read_csv(file_path):
+def read_csv(file_path) -> list:
+    """
+    Read blood glucose data from a CSV file.
+
+    Parameters:
+    - file_path (Path): Path to the CSV file. ???
+
+    Returns:
+    - list: List of GlucoseData objects.
+    """
     glucose_data_list = []
     with open(file_path, 'r') as csv_file:
         reader = csv.reader(csv_file)
@@ -77,38 +69,56 @@ def read_csv(file_path):
             glucose_data_list.append(glucose_data)
     return glucose_data_list
 
-def insert_into_database(data_list, engine):
+def insert_into_database(data_list: list, engine) -> None:
+    """
+    Insert blood glucose data into the database.
+
+    Parameters:
+    - data_list (list): List of GlucoseData objects.
+    - engine: SQLAlchemy database engine.
+    """
     Base.metadata.create_all(engine)
     with Session(engine) as session:
         for data in data_list:
-            # Check for duplicates before adding to the session
             try:
                 existing_entry = (
                     session.query(GlucoseData)
                     .filter_by(name=data.name, time_stamp=data.time_stamp)
                     .one()
                 )
-                # If entry already exists, skip adding the duplicate
                 print(f"Duplicate entry found for {data.name} at {data.time_stamp}. Skipping.")
             except NoResultFound:
-                # If no duplicate found, add the data to the session
                 session.add(data)
                 
-
         session.commit()
     print("Data added to the database.")
 
-def get_data_from_database(engine):
-    with Session(engine) as session:
-        # Query all data from the database
-        query_result = session.query(GlucoseData).all()
+def get_data_from_database(engine) -> pd.DataFrame:
+    """
+    Retrieve blood glucose data from the database.
 
-        # Convert the query result to a Pandas DataFrame
+    Parameters:
+    - engine: SQLAlchemy database engine.
+
+    Returns:
+    - pd.DataFrame: Pandas DataFrame containing blood glucose data.
+    """
+    with Session(engine) as session:
+        query_result = session.query(GlucoseData).all()
         data_frame = pd.DataFrame([(data.id, data.name, data.time_stamp, data.glucose_value) for data in query_result],
                             columns=['id', 'name', 'time_stamp', 'glucose_value'])
     return data_frame
 
-def get_last_24_hours_data (data_frame):
+def get_last_24_hours_data (data_frame: pd.DataFrame) -> pd.DataFrame:
+    """
+    Extract the last 24 hours of blood glucose data.
+
+    Parameters:
+    - data_frame (pd.DataFrame): Pandas DataFrame containing blood glucose data.
+
+    Returns:
+    - pd.DataFrame: Pandas DataFrame with the last 24 hours of data.
+    """
 
     # Ensure that 'time_stamp' column is in datetime format
     data_frame['time_stamp'] = pd.to_datetime(data_frame['time_stamp'])
@@ -124,47 +134,34 @@ def get_last_24_hours_data (data_frame):
     
     return last_24_hours_data
 
-def convert_dataframe_to_string(data_frame):
+def convert_dataframe_to_compressed_string(data_frame: pd.DataFrame) -> str:
+    """
+    Convert a Pandas DataFrame to a compressed string.
+
+    Parameters:
+    - data_frame (pd.DataFrame): Pandas DataFrame to be converted.
+
+    Returns:
+    - str: Compressed string representation of the DataFrame.
+    """
     string_data = ""
     for index, row in data_frame.iterrows():
         string_data += f"{row['time_stamp']}:{row['glucose_value']}\n"
 
     return string_data.strip()
 
-def convert_dataframe_to_compressed_string(data_frame):
+def plot_data_from_database_with_matplotlib(last_24_hours_data: pd.DataFrame) -> None:
     """
-    Convert a Pandas DataFrame to a compressed string.
+    Plot blood glucose data using Matplotlib.
 
     Parameters:
-    - data_frame (pd.DataFrame): The DataFrame to be converted.
-
-    Returns:
-    - str: Compressed string representation of the DataFrame.
+    - last_24_hours_data (pd.DataFrame): Pandas DataFrame with blood glucose data.
     """
-    # Use a shorter time format
-    data_frame['time_stamp'] = data_frame['time_stamp'].dt.strftime('%Y-%m-%d')
-
-    # Convert DataFrame to a string with a comma as a delimiter
-    string_data = data_frame.to_csv(index=False)
-
-    # Compress the string
-    compressed_data = zlib.compress(string_data.encode('utf-8'))
-
-    # Base64 encode the compressed data
-    base64_encoded_data = base64.b64encode(compressed_data).decode('utf-8')
-
-    print (base64_encoded_data)
-    print (type(base64_encoded_data))
-    return base64_encoded_data
-
-def plot_data_from_database_with_matplotlib(last_24_hours_data):
 
     if last_24_hours_data.empty:
         print("No data available in the database.")
 
     else:
-        print(last_24_hours_data)
-
         # Plot the data
         plt.figure(figsize=(10, 6))
         plt.plot(last_24_hours_data['time_stamp'], last_24_hours_data['glucose_value'], marker='o', linestyle='-', color='b')
@@ -180,7 +177,13 @@ def plot_data_from_database_with_matplotlib(last_24_hours_data):
         # Show the plot
         plt.show()
 
-def plot_data_from_database_with_plotille(last_24_hours_data):
+def plot_data_from_database_with_plotille(last_24_hours_data: pd.DataFrame) -> None:
+    """
+    Plot blood glucose data using Plotille.
+
+    Parameters:
+    - last_24_hours_data (pd.DataFrame): Pandas DataFrame with blood glucose data.
+    """
 
     if last_24_hours_data.empty:
         print("No data available in the database.")
@@ -205,71 +208,83 @@ def plot_data_from_database_with_plotille(last_24_hours_data):
         plot.set_y_limits(min(glucose_values), max(glucose_values) + 10)
         plot.plot(time_stamps, glucose_values, lc='red')
         print(plot.show(legend=True))
+ 
+def main() -> None:
+    """
+    Main function to interact with the user and execute chosen actions.
+    """
+    while True:
+        try:
+            user_input = int(input(
+                "What would you like to do?\n"
+                "1) Upload data from a csv file to the database\n"
+                "2) Plot the last 24 hours of uploaded data on the command line\n"
+                "3) Plot the last 24 hours of data with Matplotlib\n"
+                "4) Ask ChatGPT to analyze the most recent days data\n"
+                "5) Exit the program\n"
+            ))
 
+            if user_input == 1:
+                # Get data from CSV file
+                glucose_data_list = read_csv(file_path)
+
+                # Put data into database
+                if glucose_data_list:
+                    insert_into_database(glucose_data_list, MYSQL_ENGINE)
+
+            elif user_input == 2:
+                # Get the data from the database and put into a dataframe
+                client_data = get_data_from_database(MYSQL_ENGINE)
+
+                # Get just the last 24 hours of data
+                last_24_hours_data = get_last_24_hours_data(client_data)
+
+                # Plot the data from the database with Plotille
+                plot_data_from_database_with_plotille(last_24_hours_data)
+                
+            elif user_input == 3:
+                # Get the data from the database and put into a dataframe
+                client_data = get_data_from_database(MYSQL_ENGINE)
+
+                # Get just the last 24 hours of data
+                last_24_hours_data = get_last_24_hours_data(client_data)
+
+                # Plot the data from the database with Matplotlib
+                plot_data_from_database_with_matplotlib(last_24_hours_data)
+
+            elif user_input ==4:
+                # Get the data from the database and put into a dataframe
+                client_data = get_data_from_database(MYSQL_ENGINE)
+
+                # Get just the last 24 hours of data
+                last_24_hours_data = get_last_24_hours_data(client_data)
+
+                # Condense the data
+                condensed_string_data = convert_dataframe_to_compressed_string (last_24_hours_data)
+            
+                # Check the token count of the condensed data and run chat
+                token_count = num_tokens_from_string(str(condensed_string_data))
+                
+                if token_count < 128000:
+                    ai_response = chat(condensed_string_data, OPENAI_KEY)
+
+            elif user_input == 5:
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
     
 if __name__ == "__main__":
-    while True:
-        user_input = int(input(
-            "What would you like to do?\n"
-            "1) Upload data from a csv file to the database\n"
-            "2) Plot the last 24 hours of uploaded data on the command line\n"
-            "3) Plot the last 24 hours of data with Matplotlib\n"
-            "4) Ask ChatGPT to analyze the most recent days data\n"
-            "5) Exit the program\n"
-        ))
+    # Create the MySQL engine
+    DATABASE_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+    MYSQL_ENGINE = create_engine(f"mysql+mysqlconnector://root:{DATABASE_PASSWORD}@localhost/glucose_data")
 
-        if user_input == 1:
-            # Get data from CSV file
-            glucose_data_list = read_csv(file_path)
+    # OpenAI API key
+    OPENAI_KEY = os.environ.get('OPENAI_KEY')
+    openai.api_key = OPENAI_KEY
 
-            # Put data into database
-            if glucose_data_list:
-                insert_into_database(glucose_data_list, mysql_engine)
-
-        elif user_input == 2:
-            # Get the data from the database and put into a dataframe
-            client_data = get_data_from_database(mysql_engine)
-
-            # Get just the last 24 hours of data
-            last_24_hours_data = get_last_24_hours_data(client_data)
-
-            # Plot the data from the database with Plotille
-            plot_data_from_database_with_plotille(last_24_hours_data)
-            
-
-        elif user_input == 3:
-            # Get the data from the database and put into a dataframe
-            client_data = get_data_from_database(mysql_engine)
-
-            # Get just the last 24 hours of data
-            last_24_hours_data = get_last_24_hours_data(client_data)
-
-            # Plot the data from the database with Matplotlib
-            plot_data_from_database_with_matplotlib(last_24_hours_data)
-
-
-        elif user_input ==4:
-            # Get the data from the database and put into a dataframe
-            client_data = get_data_from_database(mysql_engine)
-
-            # Get just the last 24 hours of data
-            last_24_hours_data = get_last_24_hours_data(client_data)
-
-            # Condense the data
-            condensed_string_data = convert_dataframe_to_string (last_24_hours_data)
-         
-            # Check the token count of the condensed data and run chat
-            token_count = num_tokens_from_string(str(condensed_string_data))
-            
-            if token_count < 128000:
-                ai_response = chat(condensed_string_data)
-
-        elif user_input == 5:
-            break
-
-        else:
-            print("Invalid input, please try again.")
-    
+    main()
 
 
 
